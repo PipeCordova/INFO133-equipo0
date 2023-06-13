a7e171ca74d5ecd2dead16570e8156ffc7f1a1dc
SebaMonte07 <93956030+SebaMonte07@users.noreply.github.com>
2023-06-13 12:01:55 -0400

    Archivo Python Actualizado

--- Parte B/sql/TareaParteB.py
+++ Parte B/sql/TareaParteB.py
@@ -29,21 +29,21 @@ cur.execute("CREATE TABLE region (nombre_reg VARCHAR(50), nombre_pais VARCHAR(50
 
 cur.execute("CREATE TABLE comuna (ID_COM VARCHAR(50), nombre_com VARCHAR(50), poblacion INT, densidad_hab FLOAT, provincia VARCHAR(50), nombre_reg VARCHAR(50), PRIMARY KEY(ID_COM), FOREIGN KEY (nombre_reg) REFERENCES region(nombre_reg));")
 
-cur.execute("CREATE TABLE salud (ID_S VARCHAR(50), tipo VARCHAR(50), n_edificios INT, PRIMARY KEY(ID_S));")
+cur.execute("CREATE TABLE salud (ID_S VARCHAR(50), tipo VARCHAR(50), PRIMARY KEY(ID_S));")
 
-cur.execute("CREATE TABLE educacion (ID_E VARCHAR(50), tipo VARCHAR(50), n_edificios INT, PRIMARY KEY(ID_E));")
+cur.execute("CREATE TABLE educacion (ID_E VARCHAR(50), tipo VARCHAR(50), PRIMARY KEY(ID_E));")
 
-cur.execute("CREATE TABLE seguridad (ID_SEG VARCHAR(50), tipo VARCHAR(50), n_edificios INT, PRIMARY KEY(ID_SEG));")
+cur.execute("CREATE TABLE seguridad (ID_SEG VARCHAR(50), tipo VARCHAR(50), PRIMARY KEY(ID_SEG));")
 
-cur.execute("CREATE TABLE entretencion (ID_EN VARCHAR(50), tipo VARCHAR(50), n_edificios INT, PRIMARY KEY(ID_EN));")
+cur.execute("CREATE TABLE entretencion (ID_EN VARCHAR(50), tipo VARCHAR(50), PRIMARY KEY(ID_EN));")
 
-cur.execute("CREATE TABLE tiene_sal (ID_S VARCHAR(50), ID_COM VARCHAR(50), PRIMARY KEY (ID_S, ID_COM), FOREIGN KEY (ID_S) REFERENCES salud(ID_S), FOREIGN KEY (ID_COM) REFERENCES comuna(ID_COM));")
+cur.execute("CREATE TABLE tiene_sal (ID_S VARCHAR(50), ID_COM VARCHAR(50), n_edificios INT, PRIMARY KEY (ID_S, ID_COM), FOREIGN KEY (ID_S) REFERENCES salud(ID_S), FOREIGN KEY (ID_COM) REFERENCES comuna(ID_COM));")
 
-cur.execute("CREATE TABLE tiene_ed (ID_E VARCHAR(50), ID_COM VARCHAR(50), PRIMARY KEY (ID_E, ID_COM), FOREIGN KEY (ID_E) REFERENCES educacion(ID_E), FOREIGN KEY (ID_COM) REFERENCES comuna(ID_COM));")
+cur.execute("CREATE TABLE tiene_ed (ID_E VARCHAR(50), ID_COM VARCHAR(50), n_edificios INT, PRIMARY KEY (ID_E, ID_COM), FOREIGN KEY (ID_E) REFERENCES educacion(ID_E), FOREIGN KEY (ID_COM) REFERENCES comuna(ID_COM));")
 
-cur.execute("CREATE TABLE tiene_seg (ID_SEG VARCHAR(50), ID_COM VARCHAR(50), PRIMARY KEY (ID_SEG, ID_COM), FOREIGN KEY (ID_SEG) REFERENCES seguridad(ID_SEG), FOREIGN KEY (ID_COM) REFERENCES comuna(ID_COM));")
+cur.execute("CREATE TABLE tiene_seg (ID_SEG VARCHAR(50), ID_COM VARCHAR(50), n_edificios INT, PRIMARY KEY (ID_SEG, ID_COM), FOREIGN KEY (ID_SEG) REFERENCES seguridad(ID_SEG), FOREIGN KEY (ID_COM) REFERENCES comuna(ID_COM));")
 
-cur.execute("CREATE TABLE tiene_ent (ID_EN VARCHAR(50), ID_COM VARCHAR(50), PRIMARY KEY (ID_EN, ID_COM), FOREIGN KEY (ID_EN) REFERENCES entretencion(ID_EN), FOREIGN KEY (ID_COM) REFERENCES comuna(ID_COM));")
+cur.execute("CREATE TABLE tiene_ent (ID_EN VARCHAR(50), ID_COM VARCHAR(50), n_edificios INT, PRIMARY KEY (ID_EN, ID_COM), FOREIGN KEY (ID_EN) REFERENCES entretencion(ID_EN), FOREIGN KEY (ID_COM) REFERENCES comuna(ID_COM));")
 
 conn.commit() 
 conn.close()
